0e932f76cc136ab1815887d44a4caeee7b3bc55f
Elshater <Elshater.Ehab@gmail.com>
2025-01-18 04:12:18 +0200

    adding Middleware and protect admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::middleware('admin')->group(function () {
+    
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
 
